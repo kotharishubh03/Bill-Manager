1f1fcb38694a08fe0aa15cbb4ea7e143f07b8b04
kotharishubh03 <shubh.kothari1111@gmail.com>
2021-10-02 00:23:23 +0530

    Genral Addition of files and basic template of navigation done
    Signed-off-by: kotharishubh03 <shubh.kothari1111@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { faTachometerAlt, faStoreAlt, faMoneyCheck,faClipboardCheck,faParachuteBox,faPeopleCarry,faBoxOpen} from '@fortawesome/free-solid-svg-icons';
 
+import * as $ from 'jquery';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bill-manager';
+    title = 'angularbootstrap';
+    //icons
+    faTachometerAlt = faTachometerAlt;
+    faStoreAlt = faStoreAlt; 
+    faMoneyCheck = faMoneyCheck;
+    faClipboardCheck=faClipboardCheck;
+    faParachuteBox=faParachuteBox;
+    faPeopleCarry=faPeopleCarry;
+    faBoxOpen=faBoxOpen;
+    //icon-end
+    ngOnInit() {
+        //Toggle Click Function
+        $("#menu-toggle").click(function (e: { preventDefault: () => void; }) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+    }
+
 }
